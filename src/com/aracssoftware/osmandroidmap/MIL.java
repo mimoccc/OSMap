0b9b895ee0eac871b02006cdf9cb0c849e58736b
Samedli <Kamran.Samedli@gmail.com>
2012-09-05 02:33:04 +0500

    Uploading new project to GitHub

---
+++ src/com/aracssoftware/osmandroidmap/MIL.java
@@ -0,0 +1,54 @@
+package com.aracssoftware.osmandroidmap;
+
+import java.util.ArrayList;
+
+import org.osmdroid.ResourceProxy;
+import org.osmdroid.api.IMapView;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedOverlay;
+import org.osmdroid.views.overlay.OverlayItem;
+ 
+import android.graphics.Point;
+import android.graphics.drawable.Drawable;
+ 
+public  class MIL extends ItemizedOverlay<OverlayItem> {
+  
+ private ArrayList<OverlayItem> overlayItemList = new ArrayList<OverlayItem>();
+ 
+ public MIL(Drawable pDefaultMarker,
+   ResourceProxy pResourceProxy) {
+  super(pDefaultMarker, pResourceProxy);
+  // TODO Auto-generated constructor stub
+ }
+  
+ public void addItem(GeoPoint p, String title, String snippet){
+  OverlayItem newItem = new OverlayItem(title, snippet, p);
+  overlayItemList.add(newItem);
+  populate(); 
+ }
+ 
+ public boolean onSnapToItem(int arg0, int arg1, Point arg2, IMapView arg3) {
+  // TODO Auto-generated method stub
+  return false;
+ }
+ 
+ @Override
+ protected OverlayItem createItem(int arg0) {
+  // TODO Auto-generated method stub
+  return overlayItemList.get(arg0);
+ }
+ 
+ @Override
+ public int size() {
+  // TODO Auto-generated method stub
+  return overlayItemList.size();
+ }
+
+@Override
+public boolean onSnapToItem(int arg0, int arg1, Point arg2, MapView arg3) {
+	// TODO Auto-generated method stub
+	return false;
+}
+ 
+}
---
+++ src/com/aracssoftware/osmandroidmap/OSMap.java
@@ -0,0 +1,39 @@
+package com.aracssoftware.osmandroidmap;
+
+import org.osmdroid.DefaultResourceProxyImpl;
+import org.osmdroid.ResourceProxy;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import android.os.Bundle;
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
+ 
+public class OSMap extends Activity {
+  
+ MIL mIL = null;
+ 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        MapView mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
+         
+        Drawable marker=getResources().getDrawable(R.drawable.pin_red);
+        int markerWidth = marker.getIntrinsicWidth();
+        int markerHeight = marker.getIntrinsicHeight();
+        marker.setBounds(0, markerHeight, markerWidth, 0);
+         
+        ResourceProxy resourceProxy = new DefaultResourceProxyImpl(getApplicationContext());
+         
+        mIL = new MIL(marker, resourceProxy);
+        mapView.getOverlays().add(mIL);
+         
+        GeoPoint myPoint1 = new GeoPoint(0*1000000, 0*1000000);
+        mIL.addItem(myPoint1, "myPoint1", "myPoint1");
+        GeoPoint myPoint2 = new GeoPoint(50*1000000, 50*1000000);
+        mIL.addItem(myPoint2, "myPoint2", "myPoint2");
+         
+    } 
+     
+}
